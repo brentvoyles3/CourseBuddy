48129185b8ca5d1389cfbb69b8ce1224e2e37015
Brent <Brent.Voyles3@uga.edu>
2023-06-21 07:31:54 -0400

    Added course banner

--- golfcourses/src/components/LandingPage/LandingPage.js
+++ golfcourses/src/components/LandingPage/LandingPage.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import axios from 'axios';
 import './main.css';
 import Logo from '../../oof.jpg'
+import Banner from '../../resources/Images/12.png'
 import { Link } from "react-router-dom";
+
 
 class LandingPage extends Component {
     constructor(props) {
@@ -14,9 +16,15 @@ class LandingPage extends Component {
         return (
 
             <div>
-                {/* Website landing page banner */}
-                <div className="landingBanner">
-                    <img src={Logo} className="bannerImg" alt="Landing page banner"></img>
+              {/* Website landing page banner */}
+              <div className="landingBanner">
+                    <img src={Banner} className="bannerImg" alt="Landing page banner"></img>
+                </div>
+                <div className="body">
+                <a href="https://www.golfnow.com/tee-times/hot-deals#promotedcampaignsonly=false&timemax=42&timemin=10&hotdealsonly=true&date=Jun+23+2023">
+                    Golf Now!
+                    </a>
+                    <br></br>
                 </div>
                 </div>
                 );
---
+++ golfcourses/src/components/NavBar/NavBar.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import LoginButton from "../LoginButton/LoginButton";
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: false,
+        }
+        this.onLoginChange = this.onLoginChange.bind(this);
+        this.renderAuth = this.renderAuth.bind(this);
+        this.logout = this.logout.bind(this);
+    }
+
+    componentDidMount() {
+        const isLoggedIn = (sessionStorage.getItem('session')) ? true : false;
+        this.setState({
+            isLoggedIn: isLoggedIn,
+        })
+    }
+    onLoginChange(e) {
+        this.setState({
+            isLoggedIn: e.target.value
+        })
+    }
+    renderAuth() {
+        const { isLoggedIn } = this.state;
+        if (isLoggedIn) {
+            return (
+                <>
+                    <Nav.Link href="/account">Account</Nav.Link>
+                    <Nav.Link href="/" onClick={this.logout}>Logout</Nav.Link>
+                </>
+
+            );
+        } else {
+            return (
+                <>
+                    <LoginButton />
+                </>
+            );
+        }
+    }
+    render() {
+        return (
+
+            // new and improved working NavBar
+            <Navbar collapseOnSelect expand="lg" variant="dark" >
+                <Container>
+                    <Navbar.Brand href="/">StarU</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/search">Search</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>                          
+                        {this.renderAuth()}            
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        );
+    }
+}
+
+export default NavBar;
