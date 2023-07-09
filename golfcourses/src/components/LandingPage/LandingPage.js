79d325b900b704367032c742ebb26baeec01711a
Brent <Brent.Voyles3@uga.edu>
2023-07-09 11:17:38 -0400

    Added form for user input: zipcode.

--- golfcourses/src/components/LandingPage/LandingPage.js
+++ golfcourses/src/components/LandingPage/LandingPage.js
@@ -2,27 +2,55 @@ import React, { Component } from "react";
 import axios from 'axios';
 import './main.css';
 import Logo from '../../oof.jpg'
-import Banner from '../../resources/Images/12.png'
-import { Link } from "react-router-dom";
+import Banner from '../../resources/Images/123.PNG';
+import BodyPic from '../../resources/Images/a.png';
+import {Link, Routes, Route, useNavigate} from 'react-router-dom';
 
 
 class LandingPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-        }
+        this.state = {value: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    render() {
-        return (
 
+    
+    handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+
+      handleSubmit(event) {
+        alert('A zipcode was submitted: ' + this.state.value);
+        event.preventDefault();
+        window.location.href = "https://www.golfnow.com/tee-times/search#qc=GeoLocation&q=30019&sortby=Facilities.Distance.0&view=Course&date=Jul+09+2023&holes=3&radius=25&timemax=42&timemin=10&players=0&pricemax=10000&pricemin=0&longitude=-83.8795&latitude=33.9883";
+      }
+
+    render() {
+
+        return (
             <div>
               {/* Website landing page banner */}
               <div className="landingBanner">
                     <img src={Banner} className="bannerImg" alt="Landing page banner"></img>
                 </div>
-                <div className="body">
-                <a href="https://www.golfnow.com/tee-times/hot-deals#promotedcampaignsonly=false&timemax=42&timemin=10&hotdealsonly=true&date=Jun+23+2023">
-                    Golf Now!
+                <div classname="body">
+                <h2>
+                    Enter the zipcode of your destination to find top rated courses nearby!
+                </h2>
+                <div class="form">
+                <form classname="zipLanding" onSubmit={this.handleSubmit}>
+  <label>
+    Zipcode: 
+    <input type="text" value={this.state.value} onChange={this.handleChange} />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+</div>
+                </div>
+                <div className="footer">
+                <a href="https://www.golfnow.com">
+                    Book a tee time with our partners at GolfNow!
                     </a>
                     <br></br>
                 </div>
